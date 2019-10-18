79a1fc89aa9d8be4b691f613b38e87842cc65fc7
William Dixon <Wdixon@Williams-iMac.local>
2019-10-17 18:31:35 -0600

    trying pictures

--- models/Movie.js
+++ models/Movie.js
@@ -1,14 +1,15 @@
-const mongoose = require('../db/connection');
+const mongoose = require("../db/connection");
 
 const MovieSchema = new mongoose.Schema({
-    title: String,
-    writer: String,
-    director: String,
-    year: Number,
-    genre: String,
-    worthTheWatch: Boolean,
-    bluRay: Boolean,
-    dvd: Boolean
-})
+  title: String,
+  writer: String,
+  director: String,
+  year: Number,
+  genre: String,
+  worthTheWatch: Boolean,
+  bluRay: Boolean,
+  dvd: Boolean,
+  picture: String
+});
 
-module.exports = mongoose.model('Movie', MovieSchema);
+module.exports = mongoose.model("Movie", MovieSchema);
